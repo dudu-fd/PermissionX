b0fe4c9e66ec0d00595359dc69658c8163c5f33d
fanda.zeng <fandazeng@gamil.com>
2022-06-02 15:39:45 +0800

    初步功能完成

--- app/src/main/java/com/kingsun/permissionx/MainActivity.kt
+++ app/src/main/java/com/kingsun/permissionx/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.kingsun.permissionx
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.kingsun.fanda.PermissionX
+import com.kingsun.permissionx.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.makeCallBtn.setOnClickListener {
+            PermissionX.request(this,Manifest.permission.CALL_PHONE){ allGranted,deniedList ->
+                if (allGranted) {
+                    call()
+                }else {
+                    Toast.makeText(this,"You denied $deniedList",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun call(){
+        try {
+            startActivity(Intent(Intent.ACTION_CALL).apply { data = Uri.parse("tel:10086") })
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
